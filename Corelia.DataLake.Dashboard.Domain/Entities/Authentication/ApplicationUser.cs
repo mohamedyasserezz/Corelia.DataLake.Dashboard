edea65f7bf139f8db3bfa0254746ad70c848fc9f
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 00:44:31 +0300

    Add Application User and Refresh token entities

---
+++ Corelia.DataLake.Dashboard.Domain/Entities/Authentication/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Corelia.DataLake.Dashboard.Domain.Entities.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int FullName { get; set; }
+        public string? Image { get; set; }
+        public UserType UserType { get; set; }
+        public List<RefreshToken> RefreshTokens { get; set; } = [];
+    }
+}

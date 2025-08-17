c8277d19b9752bf734c21f88b8e3e7805f280c5b
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 00:47:05 +0300

    Add IUnitOfWork contract

---
+++ Corelia.DataLake.Dashboard.Domain/Contract/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Corelia.DataLake.Dashboard.Domain.Contract.Infrastructure
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IGenricRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>()
+            where TEntity : class
+            where Tkey : IEquatable<Tkey>;
+        Task<int> CompleteAsync();
+    }
+}
---
+++ Corelia.DataLake.Dashboard.Domain/Specfication/Authentication/ApplicationUserSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Corelia.DataLake.Dashboard.Domain.Entities.Authentication;
+using System.Linq.Expressions;
+
+namespace Corelia.DataLake.Dashboard.Domain.Specfication.Authentication
+{
+    public class ApplicationUserSpecification : Specification<ApplicationUser, string>
+    {
+
+        public ApplicationUserSpecification(Expression<Func<ApplicationUser, bool>>? expression) : base(expression)
+        {
+            AddIncludes();
+        }
+        private protected override void AddIncludes()
+        {
+            base.AddIncludes();
+        }
+    }
+}

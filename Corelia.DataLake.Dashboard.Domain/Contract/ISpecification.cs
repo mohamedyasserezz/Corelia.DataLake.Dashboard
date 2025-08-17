85d4f207377e77ca12f8ad32ce895b9f575647db
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 00:45:57 +0300

    Add ISpecification contract

---
+++ Corelia.DataLake.Dashboard.Domain/Contract/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Corelia.DataLake.Dashboard.Domain.Contract
+{
+    public interface ISpecification<TEntity, TKey>
+    where TKey : IEquatable<TKey>
+    {
+        public Expression<Func<TEntity, bool>>? Criteria { get; set; }
+        public List<Expression<Func<TEntity, object>>> Includes { get; set; }
+        public Expression<Func<TEntity, object>>? OrderBy { get; set; }
+        public Expression<Func<TEntity, object>>? OrderByDesc { get; set; }
+        public int Take { get; set; }
+        public int Skip { get; set; }
+        public bool IsPaginationEnabled { get; set; }
+    }
+}

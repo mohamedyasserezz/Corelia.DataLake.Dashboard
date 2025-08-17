5b32e076cf0bd4bc5bf3a9b8eaf0d793971ba978
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 02:04:43 +0300

    Add SpecificationEvaluator

---
+++ Corelia.DataLake.Dashboard.Persistance/GenericRepository/SpecificationEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using Corelia.DataLake.Dashboard.Domain.Contract;
+
+namespace Corelia.DataLake.Dashboard.Persistance.GenericRepository
+{
+    internal static class SpecificationEvaluator<TEntity, TKey>
+    where TEntity : class
+    where TKey : IEquatable<TKey>
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> query, ISpecification<TEntity, TKey> specifications)
+        {
+            if (specifications.Criteria is not null)
+                query = query.Where(specifications.Criteria);
+
+            if (specifications.OrderByDesc is not null)
+                query = query.OrderByDescending(specifications.OrderByDesc);
+            else if (specifications.OrderBy is not null)
+                query = query.OrderBy(specifications.OrderBy);
+
+            if (specifications.IsPaginationEnabled is true)
+                query = query.Skip(specifications.Skip).Take(specifications.Take);
+        
+            if (specifications.Includes is not null)
+            {
+                query = specifications.Includes.Aggregate(query, (CurrentQuery, IncludeExpression) => CurrentQuery.Include(IncludeExpression));
+            }
+
+            return query;
+        }
+    }
+}

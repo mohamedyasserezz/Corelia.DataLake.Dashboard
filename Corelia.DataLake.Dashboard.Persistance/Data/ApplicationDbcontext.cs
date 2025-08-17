0e1b2657d26a432d8caab87d0656178e07d801ca
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 02:19:38 +0300

    Add ApplicationDbcontext

---
+++ Corelia.DataLake.Dashboard.Persistance/Data/ApplicationDbcontext.cs
@@ -0,0 +1,21 @@
+﻿using Corelia.DataLake.Dashboard.Domain.Entities.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Corelia.DataLake.Dashboard.Persistance.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) :
+       IdentityDbContext<ApplicationUser>(options)
+    {
+        // User-related DbSets
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}

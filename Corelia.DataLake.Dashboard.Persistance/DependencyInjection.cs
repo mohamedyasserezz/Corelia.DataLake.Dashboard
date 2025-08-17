451d4c9cec0f106e39baad839a6ad2948a35be6e
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 02:20:12 +0300

    Add persistance DI class

---
+++ Corelia.DataLake.Dashboard.Persistance/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Corelia.DataLake.Dashboard.Domain.Contract.Infrastructure;
+using Corelia.DataLake.Dashboard.Persistance.Data;
+using Corelia.DataLake.Dashboard.Persistance.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Corelia.DataLake.Dashboard.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+
+
+
+            return services;
+        }
+    }
+}

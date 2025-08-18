189e9865607b215049cda9f23199f6f7c8f9c43c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:37:33 +0300

    Add Shared Di to program

--- Corelia.DataLake.Dashboard.Apis/Program.cs
+++ Corelia.DataLake.Dashboard.Apis/Program.cs
@@ -6,6 +6,7 @@ using HangfireBasicAuthenticationFilter;
 using Corelia.DataLake.Dashboard.Application;
 using Serilog;
 using Corelia.DataLake.Dashboard.Apis;
+using Corelia.DataLake.Dashboard.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddPersistance(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
 builder.Services.AddApis(builder.Configuration);
+builder.Services.AddShared(builder.Configuration);
 
 
 builder.Services.AddHttpContextAccessor();

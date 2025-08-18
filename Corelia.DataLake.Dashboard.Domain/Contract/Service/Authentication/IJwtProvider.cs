d97dbcf0ccad8ef783627e7eebafe381f725aff6
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:26:31 +0300

    Add Authentication abstraction

---
+++ Corelia.DataLake.Dashboard.Domain/Contract/Service/Authentication/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using Corelia.DataLake.Dashboard.Domain.Entities.Authentication;
+
+namespace Corelia.DataLake.Dashboard.Domain.Contract.Service.Authentication
+{
+    public interface IJwtProvider
+    {
+        (string token, int expiresIn) GenerateToken(ApplicationUser user, IEnumerable<string> roles);
+        string? ValidateToken(string token);
+    }
+}

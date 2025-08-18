dd0cc62e2d9073a96a12f319b54668d3c3645436
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 18:11:33 +0300

    Add RefreshTokenRequest and it's validator

---
+++ Corelia.DataLake.Dashboard.Shared/Models/Authentication/RefreshToken/RefreshTokenRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Corelia.DataLake.Dashboard.Shared.Models.Authentication.RefreshToken
+{
+    public record RefreshTokenRequest(
+        string Token,
+        string RefreshToken
+        );
+}

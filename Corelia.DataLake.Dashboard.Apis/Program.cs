b7261657071a7b49cb4646535b3584e31b90484d
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:22:21 +0300

    Add serilog and hangfire to program

--- Corelia.DataLake.Dashboard.Apis/Program.cs
+++ Corelia.DataLake.Dashboard.Apis/Program.cs
@@ -1,23 +1,71 @@
+using Corelia.DataLake.Dashboard.Persistance;
+using Hangfire;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HangfireBasicAuthenticationFilter;
+using Corelia.DataLake.Dashboard.Application;
+using Serilog;
+using Corelia.DataLake.Dashboard.Apis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+builder.Services.AddPersistance(builder.Configuration);
+builder.Services.AddServices(builder.Configuration);
+builder.Services.AddApis(builder.Configuration);
+
+
+builder.Services.AddHttpContextAccessor();
+
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(hostingContext.Configuration);
+});
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+
+
+app.UseCors();
+app.UseHangfireDashboard("/jobs", new DashboardOptions
+{
+    Authorization = [
+        new HangfireCustomBasicAuthenticationFilter{
+            User = app.Configuration.GetValue<string>("HangfireSettings:UserName"),
+            Pass = app.Configuration.GetValue<string>("HangfireSettings:Password")
+        }
+        ],
+    DashboardTitle = "Data lake project Jobs",
+
+}
+    );
+app.UseStaticFiles();
+
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.MapHealthChecks(pattern: "health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+});
 
 app.Run();

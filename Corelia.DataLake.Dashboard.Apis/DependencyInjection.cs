acb4d5b6cb965633b10c2bb02a1ceb51773ab9c5
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:36:24 +0300

    Add Apis DI

---
+++ Corelia.DataLake.Dashboard.Apis/DependencyInjection.cs
@@ -0,0 +1,101 @@
+﻿using Corelia.DataLake.Dashboard.Application.Services;
+using Corelia.DataLake.Dashboard.Application.Services.Authentication;
+using Corelia.DataLake.Dashboard.Domain.Contract.Service.Authentication;
+using Corelia.DataLake.Dashboard.Domain.Entities.Authentication;
+using Corelia.DataLake.Dashboard.Persistance.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Corelia.DataLake.Dashboard.Apis
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApis(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Identity
+            services.AddControllers();
+
+            services
+              .AddIdentity<ApplicationUser, IdentityRole>()
+              .AddEntityFrameworkStores<ApplicationDbContext>()
+              .AddDefaultTokenProviders();
+
+            services
+                .Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequiredLength = 6;
+                    options.SignIn.RequireConfirmedEmail = true;
+                    options.User.RequireUniqueEmail = true;
+
+                });
+
+            #endregion
+
+            #region JWT
+            services.AddSingleton<IJwtProvider, JwtProvider>();
+
+            services.AddOptions<JwtOptions>()
+            .BindConfiguration(JwtOptions.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+
+            var jwtSettings = configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+               .AddJwtBearer(options =>
+               {
+                   options.SaveToken = true;
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings?.Key!)),
+                       ValidIssuer = jwtSettings?.Issuer,
+                       ValidAudience = jwtSettings?.Audience
+                   };
+               });
+            #endregion
+
+            #region CORS
+            // var allowedOrgins = configuration.GetSection("AllowedOrgins").Get<string[]>();
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()    // Allow requests from any origin
+                        .AllowAnyHeader()    // Allow any headers
+                        .AllowAnyMethod();   // Allow any HTTP methods (GET, POST, etc.)
+                });
+            });
+
+            #endregion
+
+            #region SignalR
+            services.AddSignalR();
+            #endregion
+
+            #region HealthCheck
+            services.AddHealthChecks()
+                    .AddSqlServer(configuration.GetConnectionString("DefaultConnection")!, name: "database")
+                    .AddHangfire(options =>
+                    {
+                        options.MinimumAvailableServers = 1;
+                    })
+                    .AddCheck<MailProviderHealthCheck>("mail provider");
+            #endregion
+
+            return services;
+        }
+    }
+}

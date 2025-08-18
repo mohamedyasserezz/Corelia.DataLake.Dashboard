e72a909ce729ac225c66a671836f72e5009c2a1e
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:33:57 +0300

    Add register request and it's validator

---
+++ Corelia.DataLake.Dashboard.Shared/Models/Authentication/Register/RegisterRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using Corelia.DataLake.Dashboard.Shared._Common.Consts;
+using FluentValidation;
+
+namespace Corelia.DataLake.Dashboard.Shared.Models.Authentication.Register
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+
+
+            RuleFor(X => X.FullName)
+                .NotEmpty()
+                .WithMessage("Plz Add a {PropertyName}")
+                .Length(3, 100)
+                .WithMessage("{PropertyName} length should be between 3 and 100");
+
+            RuleFor(X => X.Email)
+                .NotEmpty()
+                .WithMessage(errorMessage: "Plz Add a {PropertyName}")
+                .EmailAddress();
+
+
+            RuleFor(X => X.Password)
+                .NotEmpty()
+                .WithMessage("Plz Add a {PropertyName}")
+                .Matches(RegexPatterns.Password)
+                .WithMessage("Password should be atleast 8 digits and contains LowerCase, UpperCase, NonAlphanumeric");
+            RuleFor(X => X.UserType)
+                .NotEmpty()
+                .WithMessage("Plz Add a {PropertyName}")
+                .Must(x => x == Users.Admin || x == Users.ProjectManager
+                || x == Users.Annotator)
+                .WithMessage("UserType should be either User or Admin");
+
+        }
+    }
+}

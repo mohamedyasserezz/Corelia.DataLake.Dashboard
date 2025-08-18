12c059bd292256634dbde17ee9837f04ba52ac68
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:39:59 +0300

    Add MailProviderHealthCheck

---
+++ Corelia.DataLake.Dashboard.Application/Services/MailProviderHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Corelia.DataLake.Dashboard.Shared._Common.Settings;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+namespace Corelia.DataLake.Dashboard.Application.Services
+{
+    public class MailProviderHealthCheck(IOptions<MailSettings> mailSettings) : IHealthCheck
+    {
+        private readonly MailSettings _mailSettings = mailSettings.Value;
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var smtp = new SmtpClient();
+
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls, cancellationToken);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password, cancellationToken);
+
+                return await Task.FromResult(HealthCheckResult.Healthy());
+            }
+            catch (Exception exception)
+            {
+                return await Task.FromResult(HealthCheckResult.Unhealthy(exception: exception));
+            }
+        }
+    }
+}

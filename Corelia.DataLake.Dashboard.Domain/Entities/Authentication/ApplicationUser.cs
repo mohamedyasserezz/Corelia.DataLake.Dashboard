9b5da2e02e97e4ef9c107136297bfebc1ab8c5fa
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:34:26 +0300

    Add authresponse

--- Corelia.DataLake.Dashboard.Domain/Entities/Authentication/ApplicationUser.cs
+++ Corelia.DataLake.Dashboard.Domain/Entities/Authentication/ApplicationUser.cs
@@ -4,7 +4,7 @@ namespace Corelia.DataLake.Dashboard.Domain.Entities.Authentication
 {
     public class ApplicationUser : IdentityUser
     {
-        public int FullName { get; set; }
+        public string FullName { get; set; } = string.Empty;
         public string? Image { get; set; }
         public UserType UserType { get; set; }
         public List<RefreshToken> RefreshTokens { get; set; } = [];

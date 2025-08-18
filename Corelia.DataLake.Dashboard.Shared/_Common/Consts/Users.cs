56c7dd67ec3bbcc5f93aa678744e88fa8e0c373c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:33:18 +0300

    Add consts

---
+++ Corelia.DataLake.Dashboard.Shared/_Common/Consts/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corelia.DataLake.Dashboard.Shared._Common.Consts
+{
+    public static class Users
+    {
+        public const string Admin = "Admin";
+        public const string ProjectManager = "ProjectManager";
+        public const string Annotator = "Annotator";
+    }
+}

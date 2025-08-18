ae4a0fe755ef9a5dc558518a9d0d77213253cde7
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-08-18 17:38:35 +0300

    Add Shared Di

---
+++ Corelia.DataLake.Dashboard.Shared/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Corelia.DataLake.Dashboard.Shared._Common.Settings;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Corelia.DataLake.Dashboard.Shared
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddShared(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddFluentValidationAutoValidation()
+            .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.Configure<MailSettings>(configuration.GetSection(nameof(MailSettings)));
+
+            return services;
+        }
+    }
+}

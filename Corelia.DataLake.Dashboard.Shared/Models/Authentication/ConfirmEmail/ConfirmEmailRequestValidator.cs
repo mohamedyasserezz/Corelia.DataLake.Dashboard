c0463c5ab9cb37d92c9bbd4411311e67f13cb6ad
Marwan <marwan.farhat.888@gmail.com>
2025-08-19 14:11:52 +0300

    Adding Confirm Email Validator

---
+++ Corelia.DataLake.Dashboard.Shared/Models/Authentication/ConfirmEmail/ConfirmEmailRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+public class ConfirmEmailRequestValidator : AbstractValidator<ConfirmEmailRequest>
+{
+    public ConfirmEmailRequestValidator()
+    {
+        RuleFor(X => X.Email)
+            .NotEmpty()
+            .WithMessage("Plz Add a {PropertyName}");
+
+        RuleFor(X => X.Otp)
+           .NotEmpty()
+           .WithMessage("Plz Add a {PropertyName}");
+    }
+}

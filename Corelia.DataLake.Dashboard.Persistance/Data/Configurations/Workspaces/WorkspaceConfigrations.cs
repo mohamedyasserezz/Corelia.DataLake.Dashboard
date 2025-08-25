6992cf0c895cd7b21a6243449f8a8637121c9d67
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-08-25 14:45:00 +0300

    Add Workspace Entity and it's Configurations

---
+++ Corelia.DataLake.Dashboard.Persistance/Data/Configurations/Workspaces/WorkspaceConfigrations.cs
@@ -0,0 +1,47 @@
+﻿using Corelia.DataLake.Dashboard.Domain.Entities.Workspaces;
+using Corelia.DataLake.Dashboard.Persistance.Data.Configurations.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corelia.DataLake.Dashboard.Persistance.Data.Configurations.Workspaces
+{
+	public class WorkspaceConfigrations : BaseAuditableEntityConfigurations<Workspace, int>
+	{
+		public override void Configure(EntityTypeBuilder<Workspace> builder)
+		{
+			base.Configure(builder);
+			
+			builder
+				.Property(b => b.title)
+				.IsRequired()
+				.HasMaxLength(1000);
+			
+			builder
+				.Property(b => b.color)
+				.HasMaxLength(16)
+				.IsRequired(false);
+
+
+			builder
+				.Property(b => b.description)
+				.HasMaxLength(500)
+				.IsRequired(false);
+
+			builder
+				.Property(b => b.is_archived)
+				.IsRequired()
+				.HasDefaultValue(false);
+
+			builder
+				.Property(b => b.is_personal)
+				.IsRequired()
+				.HasDefaultValue(false);
+
+		}
+	}
+}

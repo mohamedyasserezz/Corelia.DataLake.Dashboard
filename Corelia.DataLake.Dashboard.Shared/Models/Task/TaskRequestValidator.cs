e7684035cf4f3f35f81f781e7aba24de0037bc96
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-09-03 01:49:31 +0300

    Add TaskRequestValidator

---
+++ Corelia.DataLake.Dashboard.Shared/Models/Task/TaskRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Corelia.DataLake.Dashboard.Shared.Models.Task
+{
+    public class TaskRequestValidator : AbstractValidator<TaskRequest>
+    {
+        public TaskRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Id must be greater than 0");
+        }
+    }
+}

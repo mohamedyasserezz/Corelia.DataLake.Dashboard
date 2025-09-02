42d5af1bd48b352a9b93df6d37b5fa69ffdb8050
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-09-03 01:49:09 +0300

    Add TaskRequest

---
+++ Corelia.DataLake.Dashboard.Shared/Models/Task/TaskRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corelia.DataLake.Dashboard.Shared.Models.Task
+{
+    public record TaskRequest(
+        int Id
+        );
+}

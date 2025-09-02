94e1562fddc55642b39a229159810794f063c684
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-09-03 02:06:15 +0300

    Add TaskErrors

--- Corelia.DataLake.Dashboard.Shared/_Common/Errors/TasksErrors.cs
+++ Corelia.DataLake.Dashboard.Shared/_Common/Errors/TasksErrors.cs
@@ -5,6 +5,8 @@ namespace Corelia.DataLake.Dashboard.Shared._Common.Errors
 {
     public static class TasksErrors
     {
+        public static readonly Error TaskNotFound =
+            new("Task.NotFound", "Task not found", StatusCodes.Status404NotFound);
         public static readonly Error TasksNotFound =
             new("Tasks.NotFound", "No tasks found", StatusCodes.Status404NotFound);
     }
